f0d1985aaa20ac0d39fcf4bba83cffbd88abd50e
Oliver Schwendener <oliver.schwendener@hotmail.com>
2023-04-06 23:23:23 +0200

    Fixed multi select issue

--- src/Pages/Comboboxes.tsx
+++ src/Pages/Comboboxes.tsx
@@ -1,8 +1,10 @@
 import { Combobox, Option, Text } from "@fluentui/react-components";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Section } from "./Section";
 
 export const Comboboxes: FC = () => {
+    const [multiSelectValue, setMultiSelectValue] = useState<string>("");
+
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: 20, padding: 10, boxSizing: "border-box" }}>
             <Text size={600}>Comboboxes</Text>
@@ -31,17 +33,22 @@ export const Comboboxes: FC = () => {
                 title="Multiselect"
                 content={
                     <div style={{ display: "flex" }}>
-                        <Combobox placeholder="Select some items" multiselect>
-                            <Option id="item-1" key="item-1">
+                        <Combobox
+                            placeholder="Select some items"
+                            multiselect
+                            value={multiSelectValue}
+                            onOptionSelect={(_, data) => setMultiSelectValue(data.selectedOptions.join(", "))}
+                        >
+                            <Option id="item-1" key="item-1" value="Item1">
                                 Item 1
                             </Option>
-                            <Option id="item-2" key="item-2">
+                            <Option id="item-2" key="item-2" value="Item2">
                                 Item 2
                             </Option>
-                            <Option id="item-3" key="item-3">
+                            <Option id="item-3" key="item-3" value="Item3">
                                 Item 3
                             </Option>
-                            <Option id="item-4" key="item-4">
+                            <Option id="item-4" key="item-4" value="Item4">
                                 Item 4
                             </Option>
                         </Combobox>
