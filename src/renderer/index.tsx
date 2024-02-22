5d9cc30c2ef3c45659e498a999f4109410c60db5
Oliver Schwendener <oliver.schwendener@proton.me>
2024-02-22 15:16:52 +0100

    Simplified folder structure

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import { App } from "./Components";
+import { App } from "./App";
 
 document.addEventListener("DOMContentLoaded", () => {
     createRoot(document.getElementById("react-app") as HTMLDivElement).render(
