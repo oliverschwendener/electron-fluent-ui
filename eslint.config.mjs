ce77da3026e4fdcff75a7806a6358f612d716b96
Oliver Schwendener <oliver.schwendener@proton.me>
2024-06-25 06:38:03 +0200

    Added browser and node globas to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,20 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
     ...tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended),
     {
-        ignores: [
-            "coverage/",
-            "dist-main/",
-            "dist-preload/",
-            "dist-renderer/",
-            "node_modules/",
-            "electron-builder.config.js",
-        ],
+        ignores: ["coverage/", "dist-main/", "dist-preload/", "dist-renderer/", "node_modules/"],
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
     },
 ];
