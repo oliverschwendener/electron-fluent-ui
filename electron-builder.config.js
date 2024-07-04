090788f96d447d012b6dd1559db3b5d806ece555
Oliver Schwendener <oliver.schwendener@proton.me>
2024-07-04 16:01:01 +0200

    Upgraded electron-builder

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -35,9 +35,6 @@ const config = {
                 target: "msi",
             },
             {
-                target: "nsis",
-            },
-            {
                 target: "zip",
             },
             {
